a6510e2de49ae4b6b39ca585f6c7b7be775a9902
Mark Ho <mark@patristar.com>
2013-05-20 22:04:09 +0800

    Modify SecondActivity

--- AndroidTestee/src/com/patristar/androidtestee/SecondActivity.java
+++ AndroidTestee/src/com/patristar/androidtestee/SecondActivity.java
@@ -1,7 +1,14 @@
 package com.patristar.androidtestee;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class SecondActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
 
 }
